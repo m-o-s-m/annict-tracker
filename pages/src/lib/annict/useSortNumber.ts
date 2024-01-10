f055bb8fe92ded16851b18da64c39d825aaa3a5f
mosumo <mosumosumosumo@gmail.com>
2024-01-10 18:56:33 +0900

    作品の並び順を調整

--- pages/src/lib/annict/useSortNumber.ts
+++ pages/src/lib/annict/useSortNumber.ts
@@ -24,20 +24,20 @@ export function useSortNumber(entryRef: useSortNumber_LibraryEntry$key): number 
   const nextProgram = useNextProgram(entry)
 
   return useMemo(() => {
-    const year = seasonYear ?? 9999
+    const year = seasonYear ?? 1900
 
     // シーズン名が未設定の場合は最後の日にする
     if (!seasonName) {
-      return new Date(year, 12, 31).getTime() / 1000
+      return - new Date(year, 1, 1).getTime() / 1000
     }
 
     // 放送予定がない場合はシーズンの最後の日にする
     if (!nextProgram) {
-      const month = { SPRING: 6, SUMMER: 9, AUTUMN: 12, WINTER: 3 }[seasonName]
+      const month = { SPRING: 3, SUMMER: 6, AUTUMN: 9, WINTER: 0 }[seasonName]
 
-      return new Date(year, month, 31).getTime() / 1000
+      return - new Date(year, month, 1).getTime() / 1000
     }
 
-    return nextProgram.startAt.getTime() / 1000
+    return - nextProgram.startAt.getTime() / 1000
   }, [])
 }
